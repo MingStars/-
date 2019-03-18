969fc7bd8ebe02a5fbe2e54105eab7dba6048fb4
MingStar <345987671@qq.com>
2019-03-18 19:04:15 +0800

    加了一行

--- BookManager/src/cn/ming/controller/BookController.java
+++ BookManager/src/cn/ming/controller/BookController.java
@@ -57,7 +57,8 @@ public class BookController {
 		model.addAttribute("pageIndex",pageIndex);
 		model.addAttribute("pageCount", pageCount);
 		
-		
+
+
 		return "index";
 	}
 	@RequestMapping("doRemove")
